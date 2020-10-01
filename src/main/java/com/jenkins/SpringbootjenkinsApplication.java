3d55503be4dbb4cf50e08a635cfa7657c8f6e246
Harshal Mayee <harshalmayee1288@gmail.com>
2020-10-01 17:34:10 +0530

    First Commit

---
+++ src/main/java/com/jenkins/SpringbootjenkinsApplication.java
@@ -0,0 +1,24 @@
+package com.jenkins;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+public class SpringbootjenkinsApplication {
+
+	public static Logger logger= LoggerFactory.getLogger(SpringbootjenkinsApplication.class);
+
+	@PostConstruct
+	public void init(){
+		logger.info("Application Started....");
+	}
+	public static void main(String[] args) {
+		logger.info("Application executed....");
+		SpringApplication.run(SpringbootjenkinsApplication.class, args);
+	}
+
+}
