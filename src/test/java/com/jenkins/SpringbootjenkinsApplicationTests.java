b5a71721a3063640b4c441105ee568eee691aeb9
Harshal Mayee <harshalmayee1288@gmail.com>
2020-10-06 17:13:10 +0530

    Email Notification Added updated

--- src/test/java/com/jenkins/SpringbootjenkinsApplicationTests.java
+++ src/test/java/com/jenkins/SpringbootjenkinsApplicationTests.java
@@ -19,7 +19,7 @@ public class SpringbootjenkinsApplicationTests {
 	public void contextLoads() {
 		logger.info("Test Case Executed....");
 		logger.info("Test Case Executed second log");
-		logger.info("Test Case executed Email Notifications added");
+		logger.info("Test Case executed Email Notifications added updated");
 		assertEquals(true, true);
 	}
 
